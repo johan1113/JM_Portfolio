5ac5df49e16adcf0c7d385b742f96d3f59764d00
johan1113 <jsebastianm1113@gamil.com>
2020-03-19 12:34:58 -0500

    all projects updated and files compressed

--- js/project.popup.js
+++ js/project.popup.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function () {
     var btnGameMaster = document.querySelector("#btn-game-master");
     var btnAscent = document.querySelector("#btn-ascent");
     var btnTvShows = document.querySelector("#btn-tv-shows");
+    var btnArtechData = document.querySelector("#btn-artech-data");
 
 
     // When the user clicks on Game Master Project
@@ -26,6 +27,29 @@ window.addEventListener('load', function () {
         }
     }
 
+    // When the user clicks on Ascent Project
+    btnAscent.onclick = function (event) {
+
+        event.preventDefault();
+
+        // Get the modal
+        var modal = document.getElementById("modalAscent");
+
+        //open the modal
+        modal.style.display = "block";
+
+        // Play videos
+        document.querySelector(".ascent-08").play();
+        document.querySelector(".ascent-11").play();
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
+
     // When the user clicks on Tv Shows Project
     btnTvShows.onclick = function (event) {
 
@@ -36,6 +60,30 @@ window.addEventListener('load', function () {
 
         //open the modal
         modal.style.display = "block";
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
+
+    // When the user clicks on Artech Data Project
+    btnArtechData.onclick = function (event) {
+
+        
+
+        event.preventDefault();
+
+        // Get the modal
+        var modal = document.getElementById("modalArtechData");
+
+        //open the modal
+        modal.style.display = "block";
+
+        // Play videos
+        document.querySelector(".artech-data-01").play();
 
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function (event) {
