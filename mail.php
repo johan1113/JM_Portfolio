8b1a977cf380d42a68edb7051da7894e1d3d7fc4
johan1113 <jsebastianm1113@gamil.com>
2020-03-19 17:03:23 -0500

    added phpmailer plugin

--- mail.php
+++ mail.php
@@ -1,4 +1,65 @@
 <?php
+
+    require 'phpmailer/PHPMailerAutoload.php';
+
+    $mail = new PHPMailer;
+
+    // $mail->SMTPDebug = 4;                               // Enable verbose debug output
+
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+
+    $mail->Username = 'jsebastianm1113@gmail.com';                 // SMTP username
+    $mail->Password = 'sebasmedi98';                           // SMTP password
+
+    // get all input form values
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $mail->setFrom('jsebastianm1113@gmail.com', 'Portafolio - Contacto');
+    $mail->addAddress($_POST[$email]);     // Add a recipient
+    $mail->addReplyTo('jsebastianm1113@gmail.com');
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $_POST[$subject];
+
+    // prepare email body text
+    $Fields .= "Name: ";
+    $Fields .= $name;
+    $Fields .= "\n";
+
+    $Fields.= "Email: ";
+    $Fields .= $email;
+    $Fields .= "\n";
+
+    $Fields.= "Subject: ";
+    $Fields .= $subject;
+    $Fields .= "\n";
+
+    $Fields .= "Message: ";
+    $Fields .= $message;
+    $Fields .= "\n";
+    
+    $mail->Body    = $Fields;
+    $mail->AltBody = $_POST['message'];
+
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
+
+/*
+//Update credentials
+define('EMAIL', '');
+define('PASS', '');
+
 $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
@@ -28,5 +89,6 @@ $Fields .= "\n";
 
 // send email
 $success = mail($EmailTo,  $Title,  $Fields, "From:".$email); 
+*/
 ?>
 
